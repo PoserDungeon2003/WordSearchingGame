c73daf18f30eae25ab0bf14e57c55f1ae656e16b
Ngo Quang Thang <ngoquangthang2003@gmail.com>
2024-05-16 13:38:57 +0700

    add random word for puzzle

--- Assets/Scripts/Editor/BoardDataDrawer.cs
+++ Assets/Scripts/Editor/BoardDataDrawer.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEditor;
 using System;
 using UnityEditorInternal;
+using System.Text.RegularExpressions;
+using System.Linq;
 
 [CustomEditor(typeof(BoardData), false)]
 [CanEditMultipleObjects]
@@ -15,17 +17,29 @@ public class BoardDataDrawer : Editor
 
     private void OnEnable()
     {
+        InitializeReordableList(ref _dataList, "SearchWords", "Searching Words");
     }
     public override void OnInspectorGUI()
     {
         serializedObject.Update();
         DrawColumnsRowsInputFields();
         EditorGUILayout.Space();
+        ConvertToUpperButton();
 
         if (GameDataInstance.Board != null && GameDataInstance.Columns > 0 && GameDataInstance.Rows > 0)
         {
             DrawBoardTable();
         }
+
+        GUILayout.BeginHorizontal();
+
+        ClearBoardButton();
+        FillUpWithRandomLettersButton();
+
+        GUILayout.EndHorizontal();
+
+        EditorGUILayout.Space();
+        _dataList.DoLayoutList();
 
         serializedObject.ApplyModifiedProperties();
         if (GUI.changed)
@@ -93,5 +107,89 @@ public class BoardDataDrawer : Editor
             EditorGUILayout.EndVertical();
         }
         EditorGUILayout.EndHorizontal();
+    }
+
+    private void InitializeReordableList(ref ReorderableList list, string propertyName, string listLabel)
+    {
+        list = new ReorderableList(serializedObject, serializedObject.FindProperty(propertyName), true, true, true, true);
+
+        list.drawHeaderCallback = rect =>
+        {
+            EditorGUI.LabelField(rect, listLabel);
+        };
+
+        var l = list;
+
+        list.drawElementCallback = (Rect rect, int index, bool isActive, bool isFocused) =>
+        {
+            var element = l.serializedProperty.GetArrayElementAtIndex(index);
+            rect.y += 2;
+
+            EditorGUI.PropertyField(
+                new Rect(rect.x, rect.y, EditorGUIUtility.labelWidth, EditorGUIUtility.singleLineHeight), element.FindPropertyRelative("Word"), GUIContent.none);
+        };
+    }
+
+    private void ConvertToUpperButton()
+    {
+        if (GUILayout.Button("To Upper"))
+        {
+            for (int i = 0; i < GameDataInstance.Columns; i++)
+            {
+                for (int j = 0; j < GameDataInstance.Rows; j++)
+                {
+                    var errorCounter = Regex.Matches(GameDataInstance.Board[i].Row[j], @"[a-z]").Count;
+                    
+                    if (errorCounter > 0)
+                    {
+                        GameDataInstance.Board[i].Row[j] = GameDataInstance.Board[i].Row[j].ToUpper();
+                    }
+                }
+            }
+            foreach (var searchWord in GameDataInstance.SearchWords)
+            {
+                var errorCounter = Regex.Matches(searchWord.Word, @"[a-z]").Count;
+
+                if (errorCounter > 0)
+                {
+                    searchWord.Word = searchWord.Word.ToUpper();
+                }
+            }
+        }
+    }
+
+    private void ClearBoardButton()
+    {
+        if (GUILayout.Button("Clear Board"))
+        {
+            for (int i = 0; i < GameDataInstance.Columns; i++)
+            {
+                for (int j = 0; j < GameDataInstance.Rows; j++)
+                {
+                    GameDataInstance.Board[i].Row[j] = " ";
+                }
+            }
+        }
+    }
+
+    private void FillUpWithRandomLettersButton()
+    {
+        if (GUILayout.Button("Fill up With Random"))
+        {
+            for (int i = 0; i < GameDataInstance.Columns; i++)
+            {
+                for (int j = 0; j < GameDataInstance.Rows; j++)
+                {
+                    int errorCounter = Regex.Matches(GameDataInstance.Board[i].Row[j], @"[a-zA-z]").Count;
+                    string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                    int index = UnityEngine.Random.Range(0, letters.Length);
+
+                    if (errorCounter == 0)
+                    {
+                        GameDataInstance.Board[i].Row[j] = letters[index].ToString();
+                    }
+                }
+            }
+        }
     }
 }
--- Assets/Scripts/Scriptable Objects/BoardData.cs
+++ Assets/Scripts/Scriptable Objects/BoardData.cs
@@ -44,6 +44,7 @@ public class BoardData : ScriptableObject
     public int Rows = 0;
 
     public BoardRow[] Board;
+    public List<SearchingWord> SearchWords = new List<SearchingWord>();
 
     public void ClearWithEmptyString()
     {
