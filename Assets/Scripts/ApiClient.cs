389f4fe77eed8f817f461c2f0b9d9216d279aebb
Ngo Quang Thang <ngoquangthang2003@gmail.com>
2024-06-21 02:17:30 +0700

    Update api client

--- Assets/Scripts/ApiClient.cs
+++ Assets/Scripts/ApiClient.cs
@@ -108,8 +108,6 @@ public class ApiClient : MonoBehaviour
     }
 #if UNITY_EDITOR
     private readonly string _baseUrl = "https://localhost:7111";
-#else
-    private readonly string _baseUrl = "https://poserdungeon.myddns.me:7111";
 #endif
 
     public async Task<WordDataList> GetWordsAsync(int difficultyId)
